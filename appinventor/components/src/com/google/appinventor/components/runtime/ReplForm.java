ankushg/appinventor-sources

commit 8ab33ab852f742d374d870e2187040af51e1dcd8

Let the Companion Display Assets.
Arrange to fetch assets from the APK file in the Companion so it can
have a UI with images etc. Add a function to the PhoneStatus block
used to declare that the UI is loaded and assets should now be loaded
from the SD Card (because they would be loaded by the App being
developed!).

Change-Id: Ib2f407d97db9ac2fba7c7e242e3a974f2590b5f2

appinventor/components/src/com/google/appinventor/components/runtime/ReplForm.java
@@ -45,6 +45,7 @@ public class ReplForm extends Form {
   public static ReplForm topform;
   private static final String REPL_ASSET_DIR = "/sdcard/AppInventor/assets/";
   private boolean IsUSBRepl = false;
+  private boolean assetsLoaded = false;
 
   public ReplForm() {
     super();
@@ -155,6 +156,19 @@ public class ReplForm extends Form {
     File f = new File(REPL_ASSET_DIR);
     if (!f.exists())
         f.mkdirs();             // Create the directory and all parents
+  }
+
+  // We return true if the assets for the Companion have been loaded and
+  // displayed so we should look for all future assets in the sdcard which
+  // is where assets are placed for the companion.
+  // We return false until setAssetsLoaded is called which is done
+  // by the phone status block
+  public boolean isAssetsLoaded() {
+    return assetsLoaded;
+  }
+
+  public void setAssetsLoaded() {
+    assetsLoaded = true;
   }
 
 }

appinventor/aiplayapp/src/edu/mit/appinventor/aicompanion2/Screen1.scm
@@ -1,4 +1,4 @@
 #|
 $JSON
-{"YaVersion":"75","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"10","Uuid":"0","ScreenOrientation":"portrait","Scrollable":"False","Title":"MIT AICompanion","VersionCode":"207","VersionName":"2.07","$Components":[{"$Name":"HorizontalArrangement2","$Type":"HorizontalArrangement","$Version":"2","Uuid":"-1890222609","Width":"-2","$Components":[{"$Name":"Label1","$Type":"Label","$Version":"2","Uuid":"-333340163","FontSize":"12.0","Text":"Your IP Address: "},{"$Name":"iplabel","$Type":"Label","$Version":"2","Uuid":"1435778230","Text":"IP ADDRESS","Width":"-2"}]},{"$Name":"Label2","$Type":"Label","$Version":"2","Uuid":"-28249960","FontSize":"12.0","Text":"Directions: \\n1) On the Blocks Editor, press \"Connect to Device\" and select WiFi.\\n2) Type the 6-letter code or scan the QR code displayed on the Blocks Editor.\\n3) Click the 'Connect to App Inventor' button below.\\n4) If not connected in 30 seconds, click reset and repeat steps 1-4.","Width":"-2"},{"$Name":"Label6","$Type":"Label","$Version":"2","Uuid":"1263643189","Text":"        ","Width":"-2"},{"$Name":"ScanButton","$Type":"Button","$Version":"5","Uuid":"-928166843","Text":"Scan the QR Code","Width":"-2"},{"$Name":"HorizontalArrangement1","$Type":"HorizontalArrangement","$Version":"2","Uuid":"-1363356191","Width":"-2","$Components":[{"$Name":"code","$Type":"TextBox","$Version":"4","Uuid":"2091318480","Hint":"Or type the 6-letter code here.","Width":"-2"}]},{"$Name":"Button1","$Type":"Button","$Version":"5","Uuid":"933636264","Text":"Connect to App Inventor","Width":"-2"},{"$Name":"Label8","$Type":"Label","$Version":"2","Uuid":"-1656721742","TextColor":"&HFFFF0000","Visible":"False"},{"$Name":"HorizontalArrangement5","$Type":"HorizontalArrangement","$Version":"2","Uuid":"1374735927","Visible":"False","$Components":[{"$Name":"Label7","$Type":"Label","$Version":"2","Uuid":"677206225","Text":"You did not enter 6 characters, please try again.","TextColor":"&HFFFF0000"}]},{"$Name":"RetryButton","$Type":"Button","$Version":"5","Uuid":"796723612","Text":"Reset","Visible":"False","Width":"-2"},{"$Name":"HorizontalArrangement3","$Type":"HorizontalArrangement","$Version":"2","Uuid":"-1743769894","Visible":"False","$Components":[{"$Name":"Label3","$Type":"Label","$Version":"2","Uuid":"-1412894374","Text":"Or you Can Scan a Bar Code","Visible":"False"}]},{"$Name":"HorizontalArrangement4","$Type":"HorizontalArrangement","$Version":"2","Uuid":"1722789622","Visible":"False","$Components":[{"$Name":"Label4","$Type":"Label","$Version":"2","Uuid":"-1681239855","Text":"If the Blocks Editor is already running"},{"$Name":"RestartButton","$Type":"Button","$Version":"5","Uuid":"1759990306","Text":"Restart"}]},{"$Name":"ConnectFailLabel","$Type":"Label","$Version":"2","Uuid":"1331553180","Text":"Your phone does not appear to be connected to a Wireless (WiFi) network.","TextColor":"&HFFFF0000","Visible":"False"},{"$Name":"Expireblock","$Type":"VerticalArrangement","$Version":"2","Uuid":"-1616116264","AlignHorizontal":"3","Visible":"False","$Components":[{"$Name":"expirewarning","$Type":"Label","$Version":"2","Uuid":"-243410660","Text":"This version of the App Inventor Companion is not compatible with your blocks editor. You can download a new version from the Play Store","TextColor":"&HFFFF0000"},{"$Name":"MarketButton","$Type":"Button","$Version":"5","Uuid":"566867955","Text":"Go to the Play Store"}]},{"$Name":"Web1","$Type":"Web","$Version":"3","Uuid":"-1902677450","Url":"http:\/\/osiris.mit.edu\/rendezvous\/"},{"$Name":"Clock1","$Type":"Clock","$Version":"1","Uuid":"1668438698"},{"$Name":"BarcodeScanner1","$Type":"BarcodeScanner","$Version":"1","Uuid":"240579661"},{"$Name":"TinyDB1","$Type":"TinyDB","$Version":"1","Uuid":"-587391114"},{"$Name":"ActivityStarter1","$Type":"ActivityStarter","$Version":"4","Uuid":"506337611","Action":"android.intent.action.VIEW","DataUri":"market:\/\/details?id=edu.mit.appinventor.aicompanion2"},{"$Name":"PhoneStatus1","$Type":"PhoneStatus","$Version":"1","Uuid":"-315198317"}]}}
+{"YaVersion":"76","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"10","Uuid":"0","ScreenOrientation":"portrait","Scrollable":"False","Title":"MIT AICompanion","VersionCode":"207","VersionName":"2.07","$Components":[{"$Name":"HorizontalArrangement2","$Type":"HorizontalArrangement","$Version":"2","Uuid":"-1890222609","Width":"-2","$Components":[{"$Name":"Label1","$Type":"Label","$Version":"2","Uuid":"-333340163","FontSize":"12.0","Text":"Your IP Address: "},{"$Name":"iplabel","$Type":"Label","$Version":"2","Uuid":"1435778230","Text":"IP ADDRESS","Width":"-2"}]},{"$Name":"Label2","$Type":"Label","$Version":"2","Uuid":"-28249960","FontSize":"12.0","Text":"Directions: \\n1) On the Blocks Editor, press \"Connect to Device\" and select WiFi.\\n2) Type the 6-letter code or scan the QR code displayed on the Blocks Editor.\\n3) Click the 'Connect to App Inventor' button below.\\n4) If not connected in 30 seconds, click reset and repeat steps 1-4.","Width":"-2"},{"$Name":"Label6","$Type":"Label","$Version":"2","Uuid":"1263643189","Text":"        ","Width":"-2"},{"$Name":"ScanButton","$Type":"Button","$Version":"5","Uuid":"-928166843","Text":"Scan the QR Code","Width":"-2"},{"$Name":"HorizontalArrangement1","$Type":"HorizontalArrangement","$Version":"2","Uuid":"-1363356191","Width":"-2","$Components":[{"$Name":"code","$Type":"TextBox","$Version":"4","Uuid":"2091318480","Hint":"Or type the 6-letter code here.","Width":"-2"}]},{"$Name":"Button1","$Type":"Button","$Version":"5","Uuid":"933636264","Text":"Connect to App Inventor","Width":"-2"},{"$Name":"Label8","$Type":"Label","$Version":"2","Uuid":"-1656721742","TextColor":"&HFFFF0000","Visible":"False"},{"$Name":"HorizontalArrangement5","$Type":"HorizontalArrangement","$Version":"2","Uuid":"1374735927","Visible":"False","$Components":[{"$Name":"Label7","$Type":"Label","$Version":"2","Uuid":"677206225","Text":"You did not enter 6 characters, please try again.","TextColor":"&HFFFF0000"}]},{"$Name":"RetryButton","$Type":"Button","$Version":"5","Uuid":"796723612","Text":"Reset","Visible":"False","Width":"-2"},{"$Name":"HorizontalArrangement3","$Type":"HorizontalArrangement","$Version":"2","Uuid":"-1743769894","Visible":"False","$Components":[{"$Name":"Label3","$Type":"Label","$Version":"2","Uuid":"-1412894374","Text":"Or you Can Scan a Bar Code","Visible":"False"}]},{"$Name":"HorizontalArrangement4","$Type":"HorizontalArrangement","$Version":"2","Uuid":"1722789622","Visible":"False","$Components":[{"$Name":"Label4","$Type":"Label","$Version":"2","Uuid":"-1681239855","Text":"If the Blocks Editor is already running"},{"$Name":"RestartButton","$Type":"Button","$Version":"5","Uuid":"1759990306","Text":"Restart"}]},{"$Name":"ConnectFailLabel","$Type":"Label","$Version":"2","Uuid":"1331553180","Text":"Your phone does not appear to be connected to a Wireless (WiFi) network.","TextColor":"&HFFFF0000","Visible":"False"},{"$Name":"Expireblock","$Type":"VerticalArrangement","$Version":"2","Uuid":"-1616116264","AlignHorizontal":"3","Visible":"False","$Components":[{"$Name":"expirewarning","$Type":"Label","$Version":"2","Uuid":"-243410660","Text":"This version of the App Inventor Companion is not compatible with your blocks editor. You can download a new version from the Play Store","TextColor":"&HFFFF0000"},{"$Name":"MarketButton","$Type":"Button","$Version":"5","Uuid":"566867955","Text":"Go to the Play Store"}]},{"$Name":"Web1","$Type":"Web","$Version":"3","Uuid":"-1902677450","Url":"http:\/\/osiris.mit.edu\/rendezvous\/"},{"$Name":"Clock1","$Type":"Clock","$Version":"1","Uuid":"1668438698"},{"$Name":"BarcodeScanner1","$Type":"BarcodeScanner","$Version":"1","Uuid":"240579661"},{"$Name":"TinyDB1","$Type":"TinyDB","$Version":"1","Uuid":"-587391114"},{"$Name":"ActivityStarter1","$Type":"ActivityStarter","$Version":"4","Uuid":"506337611","Action":"android.intent.action.VIEW","DataUri":"market:\/\/details?id=edu.mit.appinventor.aicompanion2"},{"$Name":"PhoneStatus1","$Type":"PhoneStatus","$Version":"1","Uuid":"-315198317"}]}}
 |#

appinventor/aiplayapp/src/edu/mit/appinventor/aicompanion2/Screen1.yail
@@ -4,35 +4,15 @@ $Source $Yail
 
 (define-repl-form edu.mit.appinventor.aicompanion2.Screen1 Screen1)
 (require <com.google.youngandroid.runtime>)
-(def (Setup)
-(set-var! ipaddress  (call-component-method 'PhoneStatus1 'GetWifiIpAddress (*list-for-runtime*)
- '())
-)
+(def (badversion)
+(set-var! expired  #t)
 
-(set-and-coerce-property! 'iplabel 'Text (get-var ipaddress)
- 'text)
-
-(set-and-coerce-property! 'Button1 'Enabled #t 'boolean)
-
-(set-and-coerce-property! 'ScanButton 'Enabled #t 'boolean)
+(set-and-coerce-property! 'Expireblock 'Visible #t 'boolean)
 
 (set-and-coerce-property! 'RetryButton 'Visible #f 'boolean)
 
-(if (call-yail-primitive yail-not (*list-for-runtime* (call-component-method 'PhoneStatus1 'isConnected (*list-for-runtime*)
- '())
-)
- '( boolean)
- "not")
- (begin (set-and-coerce-property! 'ConnectFailLabel 'Visible #t 'boolean)
-
-)
- (begin (set-and-coerce-property! 'ConnectFailLabel 'Visible #f 'boolean)
-
-)
-)
-
  )
-(def expired #f)
+(def ipaddress "IPADDRESS")
 
 (def (PostData)
 (set-and-coerce-property! 'Web1 'Url (call-yail-primitive string-append (*list-for-runtime* "http://" (call-yail-primitive string-append (*list-for-runtime* "rendezvous.appinventor.mit.edu" "/rendezvous/")
@@ -68,14 +48,34 @@ $Source $Yail
  '())
 
  )
-(def ipaddress "IPADDRESS")
+(def expired #f)
 
-(def (badversion)
-(set-var! expired  #t)
+(def (Setup)
+(set-var! ipaddress  (call-component-method 'PhoneStatus1 'GetWifiIpAddress (*list-for-runtime*)
+ '())
+)
 
-(set-and-coerce-property! 'Expireblock 'Visible #t 'boolean)
+(set-and-coerce-property! 'iplabel 'Text (get-var ipaddress)
+ 'text)
+
+(set-and-coerce-property! 'Button1 'Enabled #t 'boolean)
+
+(set-and-coerce-property! 'ScanButton 'Enabled #t 'boolean)
 
 (set-and-coerce-property! 'RetryButton 'Visible #f 'boolean)
+
+(if (call-yail-primitive yail-not (*list-for-runtime* (call-component-method 'PhoneStatus1 'isConnected (*list-for-runtime*)
+ '())
+)
+ '( boolean)
+ "not")
+ (begin (set-and-coerce-property! 'ConnectFailLabel 'Visible #t 'boolean)
+
+)
+ (begin (set-and-coerce-property! 'ConnectFailLabel 'Visible #f 'boolean)
+
+)
+)
 
  )
 ;;; Screen1
@@ -84,11 +84,6 @@ $Source $Yail
 (set-and-coerce-property! 'Screen1 'Title "MIT AICompanion" 'text)
 (set-and-coerce-property! 'Screen1 'VersionCode 207 'number)
 (set-and-coerce-property! 'Screen1 'VersionName "2.07" 'text)
-)
-(define-event Screen1 Initialize()
- (set-this-form)
- ((get-var Setup))
-
 )
 (define-event Screen1 ErrorOccurred( component  functionName  errorNumber  message )
  (set-this-form)
@@ -107,6 +102,14 @@ $Source $Yail
 )
 
 ((get-var Setup))
+
+)
+(define-event Screen1 Initialize()
+ (set-this-form)
+ ((get-var Setup))
+
+(call-component-method 'PhoneStatus1 'setAssetsLoaded (*list-for-runtime*)
+ '())
 
 )
 ;;; HorizontalArrangement2

appinventor/components/src/com/google/appinventor/components/runtime/PhoneStatus.java
@@ -16,6 +16,8 @@ import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.common.YaVersion;
+import com.google.appinventor.components.runtime.Form;
+import com.google.appinventor.components.runtime.ReplForm;
 
 /**
  * Component for obtaining Phone Information. Currently supports
@@ -34,9 +36,11 @@ import com.google.appinventor.components.common.YaVersion;
 public class PhoneStatus extends AndroidNonvisibleComponent implements Component {
 
   private static Activity activity;
+  private final Form form;
 
   public PhoneStatus(ComponentContainer container) {
     super(container.$form());
+    this.form = container.$form();
     activity = container.$context();
   }
 
@@ -63,6 +67,13 @@ public class PhoneStatus extends AndroidNonvisibleComponent implements Component
         connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
     }
     return networkInfo == null ? false : networkInfo.isConnected();
+  }
+
+  @SimpleFunction(description = "Declare that we have loaded our initial assets and other assets should come from the sdcard")
+  public void setAssetsLoaded() {
+    if (form instanceof ReplForm) {
+      ((ReplForm) form).setAssetsLoaded();
+    }
   }
 
   public static String intToIp(int i) {

appinventor/components/src/com/google/appinventor/components/runtime/util/MediaUtil.java
@@ -114,7 +114,10 @@ public class MediaUtil {
     }
 
     if (form instanceof ReplForm) {
-      return MediaSource.REPL_ASSET;
+      if (((ReplForm)form).isAssetsLoaded())
+        return MediaSource.REPL_ASSET;
+      else
+        return MediaSource.ASSET;
     }
 
     return MediaSource.ASSET;

commit 384e5d904efa18e7d1767861e0ade0ddd636af16

Fix USB Repl to properly find assets.
Fix USB Repl to properly find assets. This was broken when in
8ab33ab852f742d374d870e2187040af51e1dcd8 when we permitted the
Companion to have its own assets.

Change-Id: Iaeb4008c9395d3bb8c8d08ef373603a3f4a981e7

appinventor/components/src/com/google/appinventor/components/runtime/ReplForm.java
@@ -65,6 +65,7 @@ public class ReplForm extends Form {
         PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
       formReplCommController = new ReplCommController(this);
       formReplCommController.startListening(true /*showAlert*/);
+      assetsLoaded = true;                       // we don't have any for the usb repl...
     }
   }
 